f1247340d4c058861b7cfe6df1af7fcb27e07f45
WallaceKabiru <wallacewnk@gmail.com>
2023-11-06 15:04:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ function PageTransitions(){
     for(let i = 0; i < secBtn.length; i++){
         secBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace ('active-btn', '');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
             this.className += 'active-btn';
         })
+
     }
 }
 
