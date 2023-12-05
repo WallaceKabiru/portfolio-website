d96228699a62e5cb27dcad7945dfbc4d9acd617c
WallaceKabiru <wallacewnk@gmail.com>
2023-12-05 11:06:35 +0300

    portfolio code

--- app.js
+++ app.js
@@ -13,6 +13,34 @@ function PageTransitions(){
         })
 
     }
+
+    //section active
+    allSections.addEventListener('click', (e)=>{
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected from other btns
+            secBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+
+        }
+    })
+
+    //toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',() =>{
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
 }
 
 PageTransitions();
